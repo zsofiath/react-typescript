92d34fa64f8c0b9695dd094856cf4c81fc3ef9f7
zsofi <zsofiath@gmail.com>
2022-02-05 16:24:55 +0100

    :sparkles: creating type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Todos } from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [
+    new Todo('Muradin Bronzebeard'),
+    new Todo('Arthat Menethil')
+  ];
   return (
     <div>
-      <Todos items={['ghffhh', 'poiojiojo']} />
+      <Todos items={todos} />
     </div>
   );
 }
